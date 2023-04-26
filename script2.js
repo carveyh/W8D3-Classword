866219c3fb1b95f91ec62a43e6376a5cb21fc747
carveyh <carvey.hor@gmail.com>
2023-04-26 15:22:24 -0400

    addNumbers in progress

--- script2.js
+++ script2.js
@@ -1,20 +1,38 @@
+const { get } = require("http");
 const reader = require("readline");
 const getNum = reader.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-getNum.question('Please enter a number:', addNumbers(0, 5) => {
+debugger
 
-})
+// getNum.question('Please enter a number:', addNumbers(0, 5) => {
+
+// })
 
 
 function addNumbers(sum, numsLeft, completionCallback) {
+    getNum.question('Please enter a number:', parseInt => {
+        sum += parseInt(parseInt, 10);
+        if (numsLeft == 1) {
+            getNum.close();
+            return completionCallback(sum);
+        } else {
+            console.log(sum);
+            addNumbers(sum, numsLeft - 1, completionCallback);
+        }
+        // for(i = numsLeft; i > 0; i--) {
+        //     sum += parseInt;
+        //     console.log(sum);
+        // }
 
+
+    })
 };
 
 
 
 
 
-// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
