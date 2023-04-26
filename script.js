b1497fc8f8741aa4fcca49c5c8972cca5e9f2600
carveyh <carvey.hor@gmail.com>
2023-04-26 14:36:48 -0400

    Completed Clock

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ class Clock {
         let ourTime = new Date();
 
       // 2. Store the hours, minutes, and seconds.
-        let hours = ourTime.getHours();
-        let minutes = ourTime.getMinutes();
-        let seconds = ourTime.getSeconds();
+        this.hours = ourTime.getHours();
+        this.minutes = ourTime.getMinutes();
+        this.seconds = ourTime.getSeconds();
 
       // 3. Call printTime.
         this.printTime();
 
       // 4. Schedule the tick at 1 second intervals.
-        while (true) {
-            setInterval(_tick, 1000);
-        };
+        setInterval(() => {
+          this._tick();
+        }, 1000);
         
     };
   
@@ -22,11 +22,27 @@ class Clock {
       // Format the time in HH:MM:SS
 
       // Use console.log to print it.
+      console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
     }
   
     _tick() {
       // 1. Increment the time by one second.
-      // 2. Call printTime.
+      if(this.seconds < 59) {
+        this.seconds += 1;
+      } else { 
+        this.seconds = 0;
+        if(this.minutes < 59) {
+          this.minutes += 1;
+        } else {
+          this.minutes = 0;
+          if(this.hours < 23) {
+            this.hours += 1;
+          } else {
+            this.hours = 0;
+          };
+        };
+      };
+      this.printTime();
     }
   }
   
