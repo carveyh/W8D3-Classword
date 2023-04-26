c94ae71e4b1df6b476f1bcbe4e70af77f35e142d
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-04-26 15:36:02 -0400

    finish addNumbers

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-const { get } = require("http");
+// const { get } = require("http");
 const reader = require("readline");
 const getNum = reader.createInterface({
     input: process.stdin,
@@ -13,26 +13,19 @@ debugger
 
 
 function addNumbers(sum, numsLeft, completionCallback) {
-    getNum.question('Please enter a number:', parseInt => {
-        sum += parseInt(parseInt, 10);
+    getNum.question('Please enter a number:', userNum => {
+        sum += parseInt(userNum, 10);
         if (numsLeft == 1) {
             getNum.close();
             return completionCallback(sum);
         } else {
             console.log(sum);
             addNumbers(sum, numsLeft - 1, completionCallback);
-        }
-        // for(i = numsLeft; i > 0; i--) {
-        //     sum += parseInt;
-        //     console.log(sum);
-        // }
-
-
-    })
+        };
+    });
 };
 
 
 
 
-
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+addNumbers(0, 3, (sum) => console.log(`Total Sum: ${sum}`));
